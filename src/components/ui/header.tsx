2aaa90f3a158bb6511452ac803bb97df423594b0
helmyfadlail <helmyfadlail.5@gmail.com>
2025-02-22 11:36:15 +0700

    refactor: pushhhh update

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -40,7 +40,7 @@ export const Header = () => {
             <Img src="/images/logo.png" alt="logo clinic sari dharma" className="size-14" cover />
           </Link>
 
-          <Navbar />
+          <Navbar toggleNavbar={() => console.log("test")} navbar />
           <div className="">
             <Button type="button" className="btn-primary">
               <IoSearch size={24} className="fill-white" />
