bc2fbc5ef84275ed96e5afb4d6c61767af17f7e3
helmyfadlail <helmyfadlail.5@gmail.com>
2025-08-09 18:36:59 +0700

    bug: fixing bug

--- next.config.mjs
+++ next.config.mjs
@@ -12,23 +12,22 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "back-office.saridharma.id",
-        port: "",
-        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.back-office.saridharma.id",
       },
       {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
-        pathname: "/**",
       },
       {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
-        pathname: "/**",
       },
     ],
-    domains: ["img.youtube.com", "upload.wikimedia.org"],
   },
 };
 
